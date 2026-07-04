c351b97634459908b31ceea47b3545aeac017ba8
ahsancode99 <ahsanff922@gmail.com>
2026-07-04 18:07:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// alert("hello");
+// alert("by");
+// every thing in "" is string data-type
+
+// declare variable
+var cardPrice = "10usd";
+
+console.log(cardPrice)
+
+// only declaration 
+var value ;
+
+
+var welcome = "welcome to this site!";
+console.log(welcome);
+
+// variable for number
+var numb1 = 9219; 
+console.log(numb1) 
+// data-type number
+var no1 = "hi" , no2 = "by" , no3 = "u";
+console.log(no1)
+console.log(no2)
+console.log(no1,no2,no3)
