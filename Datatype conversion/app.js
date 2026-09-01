ac71d1f47d92070c0cc18096fb97cb5605905c91
ahsancode99 <ahsanff922@gmail.com>
2026-09-01 14:57:56 +0500

    date and time

---
+++ Datatype conversion/app.js
@@ -0,0 +1,20 @@
+// string to number -----
+var num = '2.9';
+num = parseInt(num);
+console.log(num);
+
+var num1 = '3.8';
+num1 = parseFloat(num1);
+console.log(num1);
+console.log(typeof(num1));
+
+var num2 = '2.3';
+num2 = Number(num2);
+console.log(num2);
+
+// number to String-------
+var num3 = 5.4
+num3 = num3.toString()
+console.log(num3);
+
+
---
+++ Date and Time/app.js
@@ -0,0 +1,24 @@
+// var num = 5.774;
+// num = num.toFixed(5);
+// console.log(num);
+
+// var num1 = 2.565635653
+// num1 =num1.toFixed(2)
+// console.log(num1);
+
+// Date and time ///////
+
+var currentDate = new Date();
+// currentDate.setFullYear(2018);
+// currentDate.setMonth(0);
+
+
+console.log(currentDate);
+document.write(currentDate)
+
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getDay());
+// console.log(currentDate.getDate());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMilliseconds());
+// console.log(currentDate.getTime());
